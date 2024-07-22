68b2bd5b7a10ca226ff33ab777ad43977e3cf033
Waghre Abhishek <waghreabhishek@gmail.com>
2024-07-22 18:14:55 +0530

    React Project Install and Delete some file

---
+++ React_Installation/01-react-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+    return ( 
+      <div>
+        <h1> hello codewaghre </h1>
+            
+      </div>
+      
+      
+    );
+}
+
+export default App;
---
+++ React_Installation/02-React-Vite/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1> Hello code waghre</h1>
+    </>
+  )
+}
+
+export default App
