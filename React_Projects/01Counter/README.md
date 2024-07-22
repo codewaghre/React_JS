314164460c12ed7ca2f05f7bb0f8c2c5fef3f8cf
Waghre Abhishek <waghreabhishek@gmail.com>
2024-07-22 21:11:06 +0530

    counter Project

---
+++ React_Projects/01Counter/README.md
@@ -0,0 +1,14 @@
+# React + Vite
+
+##counter 
+
+counter the number on onclick event (max value 20 and Min value 0)
+
+we learn how to use usetate(), used for control the state changes in UI (DOM)
+
+to stop the max and min value we could  used stoPropogation to that cureentFunction  holder name
+
+quitck to tip 
+if we  add stopprogation to the function parameter then it will stop in UI but behind the seen its update in the consolelog
+
+
---
+++ React_Projects/01Counter/src/App.jsx
@@ -0,0 +1,48 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+
+  //Hooks
+  let [counter, setcounter] = useState(15)
+
+  // let counter = 15 // default acounter
+  
+  // Add value on click 
+  const addvalue = () => {
+    console.log("click ", counter);
+    counter = counter + 1
+    if (counter > 20) {
+      alert("Counter has Reached the maximum value")
+      console.log("Stop");
+      addvalue.stopPropogation()
+    }
+
+  setcounter(counter)
+  }
+   
+  
+  //Remove Value on click
+
+  const removeValue = () => {
+    counter = counter - 1
+    
+    if (counter < 0) {
+      alert("Counter has Reached the maximum value")
+      removeValue.stopPropogation()
+    }
+    setcounter(counter)
+  }
+
+  return (
+    <>
+      <h1>Counter Project </h1>
+      <h3> Counter value : {counter} </h3>
+      <button onClick={addvalue}> Add Value {counter} </button>
+      <button  onClick={removeValue}>Remove Value {counter }</button>
+      <p>Fotter : {counter}</p>
+    </>
+  )
+}
+
+export default App
