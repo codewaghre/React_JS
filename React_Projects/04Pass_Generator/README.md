ff949d081d17dc285a385fd438cab9e9039ce82c
Waghre Abhishek <waghreabhishek@gmail.com>
2024-07-23 17:20:53 +0530

    Password Generator

---
+++ React_Projects/04Pass_Generator/README.md
@@ -0,0 +1,35 @@
+# React + Vite
+
+## Password - Generator
+Its Generate random Password
+defualt length is (8)
+min(1) and max(100)
+user can copy the Password till 9th index value
+
+## what we leaned?
+
+
+Math.random
+usestate()
+useCallback()
+useEffect()
+useRef()
+
+--useState(), for  update the curent UI like length, number, char
+and main (chipboard called Input Password)
+
+--Usecallback() , where we genrate random password with the help of Math.random 
+str ( A to last )
+also all numbers (0- 1) and
+ some speical char
+
+ --useEffect to call another function in that 
+
+
+ useRef(), copy password using on button ref 
+
+
+
+
+
+
---
+++ React_Projects/04Pass_Generator/src/App.jsx
@@ -0,0 +1,119 @@
+import { useState, useCallback , useEffect, useRef } from "react"
+
+
+
+function App() {
+
+  // update the lengh, number, char , copy and password in UI
+  const [length, setlength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  // Use ref hook
+  const passwordRef = useRef(null)
+  
+// Generate Pass
+  const PassWordGenerator = useCallback(() => {
+
+    let pass =""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    if (numberAllowed) {
+      str += "1234567890"
+    }
+
+    if (charAllowed) {
+      str +=  "!@#$%^&*-_+=[]{}~`"
+    }
+
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+  
+  // use effect  hook to acces te password-Generator
+     useEffect(() => {
+      PassWordGenerator()
+     }, [length, numberAllowed, charAllowed, PassWordGenerator, setPassword])
+  
+
+  //copy password to clipBoard
+
+  const copyPass = useCallback(() => {
+    passwordRef.current?.select() 
+    passwordRef.current?.setSelectionRange(0,8)
+    window.navigator.clipboard.writeText(password)
+  },[password])
+  
+  return (
+    <>
+     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+            
+        />
+        <button
+         onClick={copyPass}   
+            
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e) => {setlength(e.target.value)}}
+        />
+        <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }} // previous value callback value is not smilier, compare with pre value
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )  
+              }} // previous value callback value is not smilier, compare with pre value
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
+      
+
+    </>
+  )
+}
+
+export default App
