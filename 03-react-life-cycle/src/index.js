ef2c671d3ae0cd444d32972b6f53acfa0b72b250
Waghre Abhishek <waghreabhishek@gmail.com>
2024-10-03 15:37:20 +0530

    chnage

--- 03-react-life-cycle/src/index.js
+++ 03-react-life-cycle/src/index.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Akshay from './HOC/Akshay';
 import Rohan from './HOC/Rohan';
 import Abhishek from './HOC/Abhishek';
 
-// import ProductUpdate from './classLifeCycle/ProductUpdate';
-// import LifeCycleA from './classLifeCycle/LifeCycleA';
+import ProductUpdate from './classLifeCycle/ProductUpdate';
+import LifeCycleA from './classLifeCycle/LifeCycleA';
+
+import FunctionalLifeCycle from './FunctionalLifeCycle/FunctionalLifeCycle';
+import FunctionalUpdates from './FunctionalLifeCycle/FunctionalUpdates';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <LifeCycleA/> */}
 
+    
+    {/* <LifeCycleA/> */}
     {/* <ProductUpdate/> */}
 
-    <Akshay />
+    {/* <Akshay />
     <Rohan />
-    <Abhishek />
+    <Abhishek /> */}
+
+    <FunctionalLifeCycle />
+    <FunctionalUpdates/>
   </React.StrictMode>
 );
 
