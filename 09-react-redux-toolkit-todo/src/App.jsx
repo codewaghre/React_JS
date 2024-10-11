2afd07e1d1d0b1ce18f5bc712479e509820df7bc
Waghre Abhishek <waghreabhishek@gmail.com>
2024-10-12 02:24:33 +0530

    react- todo - redux - toolkit

---
+++ 09-react-redux-toolkit-todo/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+       <h1 >
+        Learn About React Redux 
+        <AddTodo />
+        <Todos/>
+    </h1>
+        
+    </>
+  )
+}
+
+export default App
